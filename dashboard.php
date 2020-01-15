fd35c445795f0865cf85cea70e68455260de2c84
Bryle Mac <brylemp@yahoo.com>
2020-01-15 22:51:01 +0800

    basta link2 sa driver

--- dashboard.php
+++ dashboard.php
@@ -1,52 +1,54 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="css/dashboard.css">
-    <title>USC Shuttle Disbursement</title>
-  </head>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/dashboard.css">
+  <title>USC Shuttle Disbursement</title>
+</head>
 <body>
-    <div class="wrapper">
-        <div class="sidebar">
-            <h1>Dela Cruz, Juan</h1>
-            <h2>BURSAR</h2>
-            <ul>
-                <li class="active"><a href="dashboard.php">ALL</a></li>
-                <li><a href="driver.php">Driver 1</a></li>
-                <li><a href="#">Driver 2</a></li>
-                <li><a href="#">Driver 3</a></li>
-                <li><a href="#">Driver 4</a></li>
-                <li><a href="#">Driver 5</a></li>
-                <li><a href="#">Driver 6</a></li>
-                <li><a href="#">Driver 7</a></li>
-                <li><a href="#">Driver 8</a></li>
-                <li><a href="#">Driver 9</a></li>
-                <li><a href="#">Driver 10</a></li>
-                <li><a href="#">LOGOUT</a></li>
-            </ul>
-        </div>
-        <div class="main">
-        <?php
+  <div class="wrapper">
+    <div class="sidebar">
+      <h1>Dela Cruz, Juan</h1>
+      <h2>BURSAR</h2>
+      <ul>
+      <li><a href="dashboard.php">All</a></li>
+      <?php
         $servername = "localhost";
         $username = "root";
         $password = "";
-        $dbname = "ourserver";
-
+        $dbname = "ourserver"; 
         $conn = new mysqli($servername, $username, $password, $dbname);
+
         // Check connection
         if($conn->connect_error) {
+          echo '<div class="title">FAIL</div>';
           die("Connection failed: ".$conn->connect_error);
         } 
-        else{
-          echo '<div class="title">USC-TC SHUTTLE DISBURSEMENT</div>
-          <div class="date"><input type="date"></div>';
+        $sql = "SELECT * FROM DriverInformation";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()) {
+            echo '<li><a href="driver.php?driver='.$row["Driver_Name"].'">' .$row["Driver_Name"]. '</a></li>';
+          } 
+        } 
+        else {
+            echo "No record";
         }
+        $selected_driver = 0;
+      ?>
+        <li><a href="#">LOGOUT</a></li>
+      </ul>
+    </div>
+    <div class="main">
+      <div class="title">USC-TC SHUTTLE DISBURSEMENT</div>
+      <div class="date"><input type="date"></div>
+      <?php
         $sql = "SELECT * FROM DriverInformation";
         $result = $conn->query($sql);
 
@@ -60,102 +62,20 @@
                   <th></th>
                 </tr>";
           while($row = $result->fetch_assoc()) {
-              echo "<tr><td>" .$row["Driver_ID"] ."</td>";
-              echo "<td>" .$row["Total_Amount"] ."</td>";
-              echo "<td>" .$row["Driver_Status"] ."</td>";
-              echo "<td>" .$row["Collectibles"] ."</td>";
-              echo '<td><button type="button" class="btn btn-success">Disburse</button></td>
-              </tr>';
+            echo "<tr><td>" .$row["Driver_ID"] ."</td>";
+            echo "<td>" .$row["Total_Amount"] ."</td>";
+            echo "<td>" .$row["Driver_Status"] ."</td>";
+            echo "<td>" .$row["Collectibles"] ."</td>";
+            echo '<td><button type="button" class="btn btn-success">Disburse</button></td>
+            </tr>';
           }
           echo "</table>";    
         } 
         else {
             echo "No record";
         }
-        ?>
-            <!-- <div class="title">USC-TC SHUTTLE DISBURSEMENT</div>
-            <div class="date"><input type="date"></div>
-            <table>
-                <tr>
-                  <th>DRIVER ID</th>
-                  <th>TOTAL AMOUNT</th>
-                  <th>STATUS</th>
-                  <th>COLLECTIBLES</th>
-                  <th></th>
-                </tr> -->
-                <!-- <tr>
-                  <td>15101869</td>
-                  <td>1700</td>
-                  <td>Not Disbursed</td>
-                  <td>50</td>
-                  <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                  <td>15102969</td>
-                  <td>1500</td>
-                  <td>Not Disbursed</td>
-                  <td>10</td>
-                  <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                  <td>15101819</td>
-                  <td>2000</td>
-                  <td>Not Disbursed</td>
-                  <td>20</td>
-                  <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                  <td>16969696</td>
-                  <td>1100</td>
-                  <td>Not Disbursed</td>
-                  <td>40</td>
-                  <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                  <td>10023123</td>
-                  <td>1200</td>
-                  <td>Not Disbursed</td>
-                  <td>0</td>
-                  <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                  <td>12312312</td>
-                  <td>1600</td>
-                  <td>Not Disbursed</td>
-                  <td>80</td>
-                  <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td>16969696</td>
-                    <td>1100</td>
-                    <td>Not Disbursed</td>
-                    <td>40</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                  </tr>
-                  <tr>
-                    <td>10023123</td>
-                    <td>1200</td>
-                    <td>Not Disbursed</td>
-                    <td>0</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                  </tr>
-                  <tr>
-                    <td>12312312</td>
-                    <td>1600</td>
-                    <td>Not Disbursed</td>
-                    <td>80</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                  </tr>
-                  <tr>
-                    <td>12314212</td>
-                    <td>1700</td>
-                    <td>Not Disbursed</td>
-                    <td>10</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                  </tr>
-              </table> -->
-        </div>
-
+      ?>
     </div>
+  </div>
 </body>
 </html>
--- driver.php
+++ driver.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="css/driver.css">
+    <link rel="stylesheet" href="css/newdrive.css">
     <title>USC Shuttle Disbursement</title>
   </head>
 <body>
@@ -16,90 +16,74 @@
         <div class="sidebar">
             <h1>Dela Cruz, Juan</h1>
             <h2>BURSAR</h2>
+            
             <ul>
-                <li><a href="dashboard.php">ALL</a></li>
-                <li class="active"><a href="#">Driver 1</a></liclass="active">
-                <li><a href="#">Driver 2</a></li>
-                <li><a href="#">Driver 3</a></li>
-                <li><a href="#">Driver 4</a></li>
-                <li><a href="#">Driver 5</a></li>
-                <li><a href="#">Driver 6</a></li>
-                <li><a href="#">Driver 7</a></li>
-                <li><a href="#">Driver 8</a></li>
-                <li><a href="#">Driver 9</a></li>
-                <li><a href="#">Driver 10</a></li>
+                <li><a href="dashboard.php">All</a></li>
+                <?php
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "ourserver"; 
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+
+                    // Check connection
+                    if($conn->connect_error) {
+                    echo '<div class="title">FAIL</div>';
+                    die("Connection failed: ".$conn->connect_error);
+                    } 
+                    $sql = "SELECT * FROM DriverInformation";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo '<li><a href="driver.php?driver='.$row["Driver_Name"].'">' .$row["Driver_Name"]. '</a></li>';
+                    } 
+                    } 
+                    else {
+                        echo "No record";
+                    }
+
+                    $selected_driver = 0;
+                    if (isset($_GET['driver']) ) {
+                        $selected_driver = $_GET['driver']; // Get Driver Name
+                    }
+                    
+                ?>
                 <li><a href="#">LOGOUT</a></li>
             </ul>
         </div>
         <div class="main">
             <div class="title">USC-TC SHUTTLE DISBURSEMENT</div>
-            <table>
-                <tr>
-                    <th>DATE</th>
-                    <th>TOTAL AMOUNT</th>
-                    <th>STATUS</th>
-                    <th></th>
-                </tr>
-                <tr>
-                    <td>03/09/20</td>
-                    <td>1700</td>
-                    <td>Not Disbursed</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td>03/08/20</td>
-                    <td>1500</td>
-                    <td>Not Disbursed</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td>03/07/20</td>
-                    <td>2000</td>
-                    <td>Not Disbursed</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td>03/06/20</td>
-                    <td>1100</td>
-                    <td>Not Disbursed</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td>03/05/20</td>
-                    <td>1200</td>
-                    <td>Not Disbursed</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td>03/04/20</td>
-                    <td>1600</td>
-                    <td>Not Disbursed</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td>03/03/20</td>
-                    <td>1100</td>
-                    <td>Not Disbursed</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td>03/02/20</td>
-                    <td>1200</td>
-                    <td>Not Disbursed</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td>03/01/20</td>
-                    <td>1600</td>
-                    <td>Not Disbursed</td>
-                    <td><button type="button" class="btn btn-success">Disburse</button></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>COLLECTIBLES:</td>
-                    <td>150</td>
-                    <td></td>
-                </tr>
+            <?php
+                $sql = "SELECT * FROM `DriverInformation` WHERE Driver_Name='$selected_driver'";
+                $result = $conn->query($sql);
+                
+                if ($result->num_rows > 0) {
+                echo "<table>";
+                echo "<tr>
+                        <th>DATE</th>
+                        <th>TOTAL AMOUNT</th>
+                        <th>STATUS</th>
+                        <th></th>
+                    </tr>";
+                    while($row = $result->fetch_assoc()) {
+                        echo "<tr><td>" .$row["Date"] ."</td>";
+                        echo "<td>" .$row["Total_Amount"] ."</td>";
+                        echo "<td>" .$row["Driver_Status"] ."</td>";
+                        echo '<td><button type="button" class="btn btn-success">Disburse</button></td></tr>';
+                    }
+
+                    echo "<tr>
+                        <td></td>
+                        <td>COLLECTIBLES:</td>
+                        <td>150</td>
+                        <td></td>
+                    </tr>";
+                } 
+                else {
+                    echo "No record";
+                }
+            ?>
             </table>
         </div>
     </div>
