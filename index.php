54a97118e43ca1827326806373485ee3dcc8b7bb
Bryle <brylemp@yahoo.com>
2020-08-20 18:14:01 +0800

    Imporved login screen

--- index.php
+++ index.php
@@ -8,10 +8,9 @@
 
     if(isset($_SESSION["S_Error"])){
         $Alert = ""; //SHOW
-        session_destroy();
     }
     else{
-        $Alert = "d-none";
+        $Alert = "style='visibility: hidden'";
     }
 ?>
 <!doctype html>
@@ -35,45 +34,33 @@
 </div>
 <div class="container">
     <div class="row">
-        <!-- <div class="col-sm-2" style="border: solid aqua"></div> -->
-        <div class="col-sm-2"></div>
-        <!-- <div class="col-md-8" style="border: solid brown"> -->
-        <div class="col-md-8">
-            <div class="loginImage">  
+        <div class="col-xl-12 d-flex justify-content-center">
+            <form class="form-container" id="LOGINFORM" action="login.php" method="POST" autocomplete="off">
+                <div class="logo"></div>
                 <div class="loginField">
-                    <div class="loginField2">
-                        <div class="loginAlert">
-                            <div class="alert alert-danger <?php echo $Alert ?>" role="alert">
-                                Login Unsuccessful
-                            </div>
-                        </div>
-                        <div class="loginText">
-                            <form id="LOGINFORM" action="login.php" method="POST" autocomplete="off">
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" id="basic-addon1"><div class="usericon"></div></span>
-                                </div>
-                                <input type="text" class="form-control" placeholder="User Name" name="username" required="required" autocomplete="none">
-                            </div>   
-                        </div>
-                        <div class="loginText">
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" id="basic-addon1"><div class="passicon"></div></span>
-                                </div>
-                                <input type="password" class="form-control" placeholder="Password" name="password" required="required">
-                            </div> 
-                        </div>
-                        <div class="loginButton">
-                                <div class="text-center"><input type="submit" class="btn btn-light text-center" value="Login" id="button1"></div>
-                            </form>
-                        </div>
+                    <div class="alert alert-danger" role="alert" <?php echo $Alert; session_destroy(); ?>>
+                        Login Unsuccessful!
                     </div>
+                    <div class="loginText">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1"><div class="usericon"></div></span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="User Name" name="username" required="required" autocomplete="none">
+                        </div>   
+                    </div>
+                    <div class="loginText">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1"><div class="passicon"></div></span>
+                            </div>
+                            <input type="password" class="form-control" placeholder="Password" name="password" required="required">
+                        </div> 
+                    </div>
+                    <div class="text-center"><input type="submit" class="btn btn-light text-center" value="Login" id="button1"></div>
                 </div>
-            </div>
+            </form>
         </div>
-        <!-- <div class="col-sm-2"style="border: solid aqua"></div> -->
-        <div class="col-sm-2"></div>
     </div>
 </div>
 </body>
